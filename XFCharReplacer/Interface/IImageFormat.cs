cef8ca8841da04f984164e5677c450646f732f4b
EATSTEAK <bexco2010@gmail.com>
2020-03-08 23:27:33 +0900

    초기 커밋.

---
+++ XFCharReplacer/Interface/IImageFormat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+
+namespace XFCharReplacer.Interface
+{
+    public interface IImageFormat
+    {
+        int BitDepth { get; }
+
+        string FormatName { get; }
+
+        IEnumerable<Color> Load(byte[] input);
+        byte[] Save(IEnumerable<Color> colors);
+    }
+}
---
+++ XFCharReplacer/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XFCharReplacer.Format;
+using System.Text.Json;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace XFCharReplacer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length < 1)
+            {
+                Console.WriteLine("XFCharReplacer <XF Path> <Chars JSON Path> <Output XF Path> <table JSON Path>");
+            }
+            var fileStream = File.Open(args[0], FileMode.Open);
+            var xf = new XF(fileStream);
+            var chars = JsonDocument.Parse(File.Open(args[1], FileMode.Open));
+            var exists = new Dictionary<char, XF.CharacterMap>();
+            var notExists = new List<char>();
+            foreach (JsonElement c in chars.RootElement.EnumerateArray())
+            {
+                var charStr = c.GetString().Trim('\n', '\r');
+                var character = charStr.ToCharArray();
+                if (character.Length > 0 && xf.dicGlyphLarge.ContainsKey(character[0]))
+                {
+                    exists[character[0]] = xf.dicGlyphLarge[character[0]];
+                } else if (character.Length > 0)
+                {
+                    notExists.Add(character[0]);
+                }
+            }
+            Console.WriteLine("Found " + exists.Count + " exist chars, " + notExists.Count + " non-exist chars.");
+            Console.WriteLine("Replacing unused chars to non-exist chars. This may take a while...");
+            notExists.Sort();
+            var index = 0;
+            var tbl = new Dictionary<string, char>();
+            var editedDicGlyphLarge = new Dictionary<char, XF.CharacterMap>();
+            foreach (KeyValuePair<char, XF.CharacterMap> pair in xf.dicGlyphLarge)
+            {
+                if(index >= notExists.Count)
+                {
+                    break;
+                }
+                if (((int) pair.Key) >= 0x4E00 && !exists.ContainsKey(pair.Value.code_point) && xf.lstCharSizeInfoLarge[pair.Value.code_point].char_width >= 13 && xf.lstCharSizeInfoLarge[pair.Value.code_point].char_height >= 13)
+                {
+                    tbl.Add(pair.Key + "", notExists[index]);
+                    // TO-DO Replacing unused chars to non-exists chars.
+                    RectangleF rectf = new RectangleF(pair.Value.ImageOffsetX, pair.Value.ImageOffsetY, 13, 13);
+                    Bitmap image = pair.Value.ColorChannel == 0 ? xf.image_0 : pair.Value.ColorChannel == 1 ? xf.image_1 : xf.image_2;
+                    for(int xP=pair.Value.ImageOffsetX;xP< pair.Value.ImageOffsetX+xf.lstCharSizeInfoLarge[pair.Value.code_point].char_width;xP++)
+                    {
+                        for(int yP=pair.Value.ImageOffsetY;yP < pair.Value.ImageOffsetY+xf.lstCharSizeInfoLarge[pair.Value.code_point].char_height;yP++)
+                        {
+                            image.SetPixel(xP, yP, Color.Transparent);
+                        }
+                    }
+                    Graphics g = Graphics.FromImage(image);
+                    g.SmoothingMode = SmoothingMode.AntiAlias;
+                    g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+                    g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                    editedDicGlyphLarge.Add(pair.Key, new XF.CharacterMap
+                    {
+                        char_size = (ushort)((149 & 0x3FF) | ((Convert.ToUInt16(13) & 0x3F) << 10)),
+                        code_point = pair.Value.code_point,
+                        image_offset = pair.Value.image_offset
+                    });
+                    xf.lstCharSizeInfoLarge[pair.Value.code_point].char_height = 13;
+                    xf.lstCharSizeInfoLarge[pair.Value.code_point].char_width = 13;
+                    g.DrawString(notExists[index] + "", new Font("NanumBarunGothic", 10), Brushes.White, rectf);
+                    g.Flush();
+                    if (pair.Value.ColorChannel == 0)
+                    {
+                        xf.image_0 = image;
+                    } else if(pair.Value.ColorChannel == 1)
+                    {
+                        xf.image_1 = image;
+                    } else
+                    {
+                        xf.image_2 = image;
+                    }
+                    index++;
+                }
+            }
+            foreach (KeyValuePair<char, XF.CharacterMap> newPair in editedDicGlyphLarge)
+            {
+                xf.dicGlyphLarge[newPair.Key] = newPair.Value;
+            }
+            var newFile = File.Create(args[2]);
+            xf.Save(newFile);
+            Console.WriteLine("Char Replaced. Generating Table...");
+            var fileWriter = new StreamWriter(args[3], false, Encoding.UTF8, 8192);
+            var options = new JsonSerializerOptions();
+            options.Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
+            var tblSerialized = JsonSerializer.Serialize(tbl, options);
+            fileWriter.WriteLine(tblSerialized);
+            fileWriter.Close();
+            Console.WriteLine("Table Generated.");
+        }
+    }
+}
